682b9fbee2b57c58415f1a3a5dec4b7dc16604c5
egovictor <egovictorc@outlook.com>
2024-08-30 19:27:27 +0100

    feat: add stepper_sliding_screen working code

---
+++ esp/stepper_working_review/stepper_working_review.ino
@@ -0,0 +1,359 @@
+
+#include <WiFi.h>
+#include <ArduinoJson.h>
+
+// const char* ssid = "KVRENO";
+// const char* password = "000111000";
+const char* ssid = "Ms";
+const char* password = "@12345678#";
+
+NetworkServer server(80);  //start a server on port 80
+
+// #include <AccelStepper.h>
+
+// Define motor interface type
+// #define MotorInterfaceType 1  // 1 means Driver type (DIR, STEP)
+
+// Define motor connections and motor interface type
+#define stepPin 14  // STEP pin
+#define dirPin 13   // DIR pin
+
+// Create an instance of AccelStepper
+// AccelStepper stepper(MotorInterfaceType, stepPin, dirPin);
+
+#define stepPin 14  //Pull -ve
+#define dirPin 13   //DIR -ve
+#define limiterPin 5
+#define limiterPin_2 4
+int pulse_delay = 1000;
+
+int mode = 1;
+double factor = 0.000251;
+// double factor = 0.000601;
+double initialPosition = 0.000000;
+double destination = 0.000000;
+int speed = 1;
+boolean isInitialized = false;
+// int stepperDelay = 3000;
+int stepsPerRevolution = 1000;
+int stepperDelay = 4500;
+
+void setup() {
+  // put your setup code here, to run once:
+  // declare pins as output
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+
+  //limiterPin pins
+  pinMode(limiterPin, OUTPUT);    // limiterPin mode
+  pinMode(limiterPin_2, OUTPUT);  // limiterPin mode
+
+  //initialize serial communication
+  Serial.begin(115200);
+  Serial.println("--------- Serial communicatiton started on 115200 ------------------ ");
+
+  delay(10);
+  Serial.println("------------- Connecting to Provisioned WIFI ------------------ ");
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected.");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.begin();
+  //Serial.println("--------- Initialization Completed ------------------ ");
+  // Start initialization
+
+  // Set a reasonable maximum speed and smoother acceleration
+  // stepper.setMaxSpeed(2000);       // Lower maximum speed for smoothness
+  // stepper.setAcceleration(1000);   // Moderate acceleration for smoother ramp-up
+
+  // Set the initial target position (number of steps to move)
+  // stepper.moveTo(2000);  // Move 2000 steps forward
+  resetMotorPosition();
+}
+
+
+void loop() {
+  NetworkClient client = server.accept();  // listen for incoming clients
+
+  if (client) {
+    Serial.println("New Client Connected");
+
+    // Read the first line of the request
+    String request = client.readStringUntil('\r');
+    Serial.println(request);
+
+    // Clear out any remaining HTTP headers
+    client.flush();
+
+    // Initialize JSON response
+    String jsonResponse = "{";
+
+    // Extract query parameters from GET request
+    if (request.startsWith("GET")) {
+      int paramsStart = request.indexOf('?');  // Find the start of the query string
+
+      if (paramsStart != -1) {
+        // Extract the query string
+        String queryString = request.substring(paramsStart + 1, request.indexOf(' ', paramsStart));
+        // Serial.println("Query string: " + queryString);
+
+        // Parse the query parameters (split by '&' and '=')
+        String param;
+        while (queryString.length() > 0) {
+          int paramEnd = queryString.indexOf('&');
+
+          if (paramEnd == -1) {
+            param = queryString;
+            queryString = "";
+          } else {
+            param = queryString.substring(0, paramEnd);
+            queryString = queryString.substring(paramEnd + 1);
+          }
+
+          // Split parameter into name and value
+          int separatorIndex = param.indexOf('=');
+          if (separatorIndex != -1) {
+            String paramName = param.substring(0, separatorIndex);
+            String paramValue = param.substring(separatorIndex + 1);
+            // Serial.println("Param: " + paramName + " = " + paramValue);
+
+            // set values
+
+            if (paramName == "destination") {
+              destination = paramValue.toDouble();
+            } else if (paramName == "speed") {
+              speed = paramValue.toInt();
+              setFactor(speed);
+            }
+
+            // Add to JSON response
+            jsonResponse += "\"" + paramName + "\": \"" + paramValue + "\",";
+          }
+        }
+      }
+    }
+
+    // control motor movement and send command through serial
+    move();
+    // Remove trailing comma and close JSON object
+    if (jsonResponse.endsWith(",")) {
+      jsonResponse = jsonResponse.substring(0, jsonResponse.length() - 1);
+    }
+    jsonResponse += "}";
+
+    // Send JSON response back to the client
+    client.println("HTTP/1.1 200 OK");
+    client.println("Content-Type: application/json");
+    client.println("Connection: close");
+    client.println();
+    client.println(jsonResponse);
+
+    // Close the connection
+    client.stop();
+    //Serial.println("Client Disconnected");
+  }
+}
+
+
+void setFactor(int speed) {
+  factor = speed * factor;
+  // Serial.println(" factor ");
+  // Serial.println(factor, 6);
+  // Serial.println(" ");
+}
+
+void move() {
+  // control motor movement
+  //these four lines result in 1 step
+  //max value = 255, max speed = 5 min value = 0
+
+  // delayMicroseconds(60 );                        //delay in milliseconds
+  // analogWrite(stepPin, 0);
+  // delayMicroseconds(30);
+
+  boolean isLimiting = digitalRead(limiterPin) == 1 || digitalRead(limiterPin_2) == 1;
+
+  // Serial.println(digitalRead(limiterPin));
+  if (initialPosition < destination) {
+    // digitalWrite(dirPin, LOW);
+    // delayMicroseconds(pulse_delay);
+    while (initialPosition <= destination && !isLimiting) {
+      makeOneRevolution(1);
+      Serial.println(initialPosition, 6);  //send position to serial monitor / output program
+      initialPosition = initialPosition + factor;
+      isLimiting = digitalRead(limiterPin) == 1 || digitalRead(limiterPin_2) == 1;
+    }
+  } else {
+    // digitalWrite(dirPin, HIGH);
+    while (initialPosition >= destination && !isLimiting) {
+      //rotate left
+      makeOneRevolution(0);
+      // move backward
+      Serial.println(initialPosition, 6);  //send position to serial monitor / output program
+      initialPosition = initialPosition - factor;
+      isLimiting = digitalRead(limiterPin) == 1 || digitalRead(limiterPin_2) == 1;
+    }
+  }
+  // Stop motor rotation
+  stopMotorRotation();
+  /*
+  Serial.print("\nDestination ");
+  Serial.print(destination);
+
+  Serial.print("\ninitialPosition ");
+  Serial.print(initialPosition);
+  */
+}
+
+
+void resetMotorPosition() {
+  Serial.print("Initializing ");
+  // digitalWrite(limiterPin, LOW);
+
+  //rotate left
+  // digitalWrite(dirPin, HIGH);
+  // check if motor has moved to start position: 0
+  while (digitalRead(limiterPin) != 1) {
+    // start motor anti clokwise rotation
+    makeOneRevolution(0);
+    // moveBackward();
+
+    Serial.print(".");
+    //delay(500);
+  }
+  Serial.println("");
+  Serial.println("Inititalized");
+  // Serial.println("\nInititalized");
+  stopMotorRotation();
+}
+
+
+void makeOneRevolution(int direction) {
+  // int stepperDelay = 4500;
+
+  if (direction == 0) {
+    //stepper.moveTo(stepper.currentPosition() - 5000);  // Increment the target position
+    // moveBackward();
+    digitalWrite(dirPin, LOW);
+    moveNow();
+  } else {
+    // stepper.moveTo(stepper.currentPosition() + 5000);  // Increment the target position
+    // moveForward();
+    digitalWrite(dirPin, HIGH);
+    moveNow();
+  }
+  // if (stepperDelay >= 2500) {
+  //   stepperDelay -= 100;
+  // }
+  // stepper.run();  // Move the motor towards the target position
+}
+
+
+
+void moveNow() {
+  int stepperDelay = 5000;
+  // for (int x = 0; x < stepsPerRevolution; x++) {
+    digitalWrite(stepPin, HIGH);
+    delayMicroseconds(stepperDelay);  //700
+    digitalWrite(stepPin, LOW);
+    delayMicroseconds(stepperDelay / 2);  //700
+                                          // delayMicroseconds(stepperDelay / 2);  //700
+
+    // if (stepperDelay >= 2500) {
+    //   stepperDelay -= 100;
+    // }
+  // }
+}
+
+
+
+// void moveForward() {
+
+//   // move(stepperDelay * 2);
+//   digitalWrite(dirPin, HIGH);
+
+//   digitalWrite(stepPin, HIGH);
+//   delayMicroseconds(stepperDelay);  //700
+//   digitalWrite(stepPin, LOW);
+//   delayMicroseconds(stepperDelay / 2);  //700
+//                                         // delayMicroseconds(stepperDelay / 2);  //700
+//                                         // Serial.println(initialPosition);
+// }
+
+// void moveBackward() {
+//   // int forwardDelay = 3000;
+
+//   // move(stepperDelay * 2);
+//   digitalWrite(dirPin, LOW);
+
+//   digitalWrite(stepPin, HIGH);
+//   // delayMicroseconds(stepperDelay * 2);  //700
+//   delayMicroseconds(stepperDelay * 2);  //700
+//   digitalWrite(stepPin, LOW);
+//   // delayMicroseconds(3000);  //700
+//   delayMicroseconds(stepperDelay / 2);  //700
+//                                         // delayMicroseconds(stepperDelay);  //700
+// }
+
+
+
+/*
+void makeOneRevolution() {
+  for(int i = 0; i < pulse_delay; i++) {
+    digitalWrite(stepPin, HIGH);
+    //delay(10000);
+    delayMicroseconds(pulse_delay);
+    digitalWrite(stepPin, LOW);
+    // delayMicroseconds(pulse_delay);
+    delayMicroseconds(pulse_delay);
+    //delay(10000);
+  }
+}
+*/
+
+
+
+// void makeOneRevolution() {
+//     digitalWrite(stepPin, HIGH);
+//     //delay(10000);
+//     delayMicroseconds(pulse_delay);
+//     digitalWrite(stepPin, LOW);
+//     delayMicroseconds(pulse_delay);
+//     //delay(10000);
+// }
+
+
+/*
+void resetMotorPosition() {
+  isInitialized = false;
+  Serial.print("Initializing ");
+  // check if motor has moved to start position: 0
+  while (!isInitialized) {
+    Serial.print(".");
+    delay(500);
+    if (digitalRead(limiterPin) == HIGH) {
+      isInitialized = true;
+    }
+  }
+  Serial.println("\nInititalized");
+}
+*/
+
+void stopMotorRotation() {
+  digitalWrite(stepPin, LOW);
+  //stepper.stop();
+}
--- stepper_test_acceleration/stepper_test_acceleration.ino
+++ stepper_test_acceleration/stepper_test_acceleration.ino
@@ -2,9 +2,9 @@
 const int stepPin = 14;
 const int dirPin = 13;
 int stepsPerRevolution = 1000;
-#define ledPin 5
+#define ledPin 17
 
-double initialPosition =  0.000000;
+double initialPosition = 0.000000;
 // const int enPin = 8;
 void setup() {
   Serial.begin(115200);
@@ -20,16 +20,22 @@ void setup() {
   digitalWrite(dirPin, LOW);
 }
 
-int stepperDelay = 6000;
 void loop() {
+  // int stepperDelay = 5000;
+  int stepperDelay = 4500;
+
   // put your main code here, to run repeatedly:
   if (digitalRead(ledPin) == 0) {
-    moveForward();
+    moveForward(stepperDelay);
   } else {
 
-    moveBackward();
+    moveBackward(stepperDelay);
   }
   //  delay(1000);  // Wait for 1 second before repeating or switching directions
+  // if (stepperDelay >= 3000) {
+  if (stepperDelay >= 2500) {
+    stepperDelay -= 100;
+  }
 }
 
 
@@ -41,21 +47,17 @@ void move(int stepperDelay) {
     delayMicroseconds(stepperDelay / 2);  //700
     // delayMicroseconds(stepperDelay / 2);  //700
     Serial.println(initialPosition);
-
-    if(stepperDelay >= 4500) {
-      stepperDelay -= 100;
-    } 
   }
 }
 
-void moveForward() {
+void moveForward(int stepperDelay) {
   // int forwardDelay = 3000;
   digitalWrite(dirPin, HIGH);
 
   move(stepperDelay);
 }
 
-void moveBackward() {
+void moveBackward(int stepperDelay) {
   // int forwardDelay = 3000;
 
   digitalWrite(dirPin, LOW);
